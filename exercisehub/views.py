c6f94f87d54a2d8950fa74b63b126196757e08fb
Luka <lgurjidze@gmail.com>
2024-03-10 21:09:19 +0400

    allowed user to access profile

--- exercisehub/views.py
+++ exercisehub/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from exercisehub.serializers import ProfileSerializer
 from .models import Profile
 # Create your views here.
@@ -12,4 +13,23 @@ class ProfileViewSet(ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        return [IsAdminUser()]
     
+
+    @action(detail=False, methods=['GET', 'PUT'], permission_classes=[IsAuthenticated])
+    def me(self, request):      
+        profile = Profile.objects.get(user_id=request.user.id)
+
+        if request.method == 'GET':
+            serializer = ProfileSerializer(profile)
+            return Response(serializer.data)
+        elif request.method == 'PUT':
+            serializer = ProfileSerializer(profile, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+
